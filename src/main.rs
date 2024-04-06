a045e4d83a5de4ca7e98c481513d335f4d11a56e
satler-git <mitsclatw24@mbr.nifty.com>
2024-04-06 05:21:01 +0000

    chronoで比較できるように

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use chrono::{DateTime, FixedOffset, NaiveTime, Utc};
+
+/// 与えられたNaiveTimeと現在の時刻を比較します
+/// * `end_time` - 比較対象の時間
+fn comp_end_time(end_time: NaiveTime) -> bool {
+    // let end_time: NaiveTime = NaiveTime::from_hms_opt(13, 5, 0).unwrap();
+    let now_utc: DateTime<Utc> = Utc::now();
+    let now: DateTime<FixedOffset> = now_utc.with_timezone(&FixedOffset::east_opt(9*3600).unwrap());
+    let now_naive: NaiveTime = now.time();
+    now_naive > end_time
+}
+
 fn main() {
-    println!("Hello, world!");
+    print!("{}", comp_end_time(NaiveTime::from_hms_opt(13, 5, 0).unwrap()));
 }
