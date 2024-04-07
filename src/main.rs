1f940de6f47c1b991dca9d62e50bb04cc7325551
satler-git <mitsclatw24@mbr.nifty.com>
2024-04-07 04:22:11 +0000

    confyから読み込むように

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 use chrono::{DateTime, FixedOffset, NaiveTime, Utc};
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct MyConfig {
+    api_key: String,
+    end_time: [u32; 3],
+}
+
+/// `MyConfig` implements `Default`
+impl ::std::default::Default for MyConfig {
+    fn default() -> Self { Self {
+            api_key: "".into(),
+            end_time: [13, 5, 0],
+        }
+    }
+}
 
 /// 与えられたNaiveTimeと現在の時刻を比較します
 /// * `end_time` - 比較対象の時間
@@ -10,6 +26,8 @@ fn comp_end_time(end_time: NaiveTime) -> bool {
     now_naive > end_time
 }
 
-fn main() {
-    print!("{}", comp_end_time(NaiveTime::from_hms_opt(13, 5, 0).unwrap()));
+fn main() -> Result<(), confy::ConfyError> {
+    let cfg: MyConfig  = confy::load("tt", "tt")?;
+    println!("{}", comp_end_time(NaiveTime::from_hms_opt(cfg.end_time[0], cfg.end_time[1], cfg.end_time[2]).unwrap()));
+    Ok(())
 }
